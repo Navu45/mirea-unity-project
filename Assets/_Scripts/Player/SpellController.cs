b456851c1ef2fe666554371d0186b1381af513b0
Alexey Yamoncheryaev <jamoncheryev1@gmail.com>
2022-05-25 00:20:55 +0300

    [fix] level 1 and vfx

--- Assets/_Scripts/Player/SpellController.cs
+++ Assets/_Scripts/Player/SpellController.cs
@@ -4,14 +4,9 @@ using UnityEngine;
 
 public class SpellController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public SpellContext spellContext;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
         
     }
--- Assets/_Scripts/SpellContext.cs
+++ Assets/_Scripts/SpellContext.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(menuName = "SpellContext", fileName = "NewSpells", order = 51)]
 public class SpellContext : ScriptableObject
 {
     public List<Spell> spellPrefabs;
